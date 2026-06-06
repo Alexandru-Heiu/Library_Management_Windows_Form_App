d01ddfa9e17e85f75401b973db981f4d03c2d0c2
Alexandru Heiu <sasha.heiu@gmail.com>
2026-06-06 19:25:13 +0300

    feat: initialize solution structure with layered architecture #1

---
+++ Helpers/DatabaseHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Helpers
+{
+    public static class DatabaseConfig
+    {
+        public static string ConnectionString =>
+            "Server=localhost\\SQLEXPRESS;Database=LibraryDB;Trusted_Connection=True;TrustServerCertificate=True;";
+    }
+}
